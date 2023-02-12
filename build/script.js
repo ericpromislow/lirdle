5ad162b7ca3413e2db2bb9035d56edec1c31a61b
Eric Promislow <eric.promislow@suse.com>
2023-02-11 17:40:01 -0800

    Initing

---
+++ build/script.js
@@ -0,0 +1,8 @@
+import { WORDS } from "./words.js";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(rightGuessString)
