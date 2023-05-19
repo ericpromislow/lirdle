2d091b9650b3f266e3c6e8b435b15c098c05fbd7
Eric Promislow <eric.promislow@suse.com>
2023-05-18 23:18:41 -0700

    No longer need the isPossibleWord function

--- tests/solver.spec.js
+++ tests/solver.spec.js
@@ -1,6 +1,5 @@
-import * as solver from '../build/solver.js';
-import {evalPossibleWords, evaluateGuess, getSolverData, isPossibleWord, scoreMakesSense} from "../build/solver.js";
-import {WORDS} from "../build/words";
+import { evalPossibleWords, evaluateGuess, getSolverData, scoreMakesSense, updateSolver } from "../build/solver.js";
+import { WORDS } from "../build/words";
 
 describe('solver tests', () => {
     describe('evaluate guess', () => {
@@ -11,58 +10,18 @@ describe('solver tests', () => {
     })
     describe('reduced word list', () => {
         const currentWordList = ['taste', 'waste', 'wedge', 'llama', 'mango'];
-        describe('isPossibleWord', () => {
-            test('all green', () => {
-                expect(solver.isPossibleWord('green'.split(''), [2,2,2,2,2], 'green')).toBeTruthy();
-                expect(solver.isPossibleWord('green'.split(''), [2,2,2,2,2], 'greed')).toBeFalsy();
-            });
-            test('all black', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [0,0,0,0,0], 'fghij')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [0,0,0,0,0], 'fghie')).toBeFalsy();
-            });
-            test('all yellow', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [1,1,1,1,1], 'cdeab')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [1,1,1,1,1], 'cdeag')).toBeFalsy();
-            });
-            test('green and black', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [2,0,2,0,2], 'axcxe')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [2,0,2,0,2], 'abcxe')).toBeFalsy();
-                expect(solver.isPossibleWord('abcde'.split(''), [2,0,2,0,2], 'yxcxe')).toBeFalsy();
-            });
-            test('yellow and black', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [1,0,1,0,1], 'cxexa')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [1,0,1,0,1], 'axexc')).toBeFalsy();
-                expect(solver.isPossibleWord('aacde'.split(''), [1,0,0,0,0], 'mnoap')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [1,0,1,0,1], 'ebaxc')).toBeFalsy();
-                expect(solver.isPossibleWord('aacde'.split(''), [0,1,0,0,0], 'mnoap')).toBeTruthy();
-            });
-            test('green and yellow', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [2,1,2,1,2], 'adcbe')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [2,1,2,1,2], 'abcxe')).toBeFalsy();
-                expect(solver.isPossibleWord('abcde'.split(''), [2,1,2,1,2], 'axcye')).toBeFalsy();
-            });
-            test('all three colors', () => {
-                expect(solver.isPossibleWord('abcde'.split(''), [0, 2, 1, 1, 0], 'xbdcy')).toBeTruthy();
-                expect(solver.isPossibleWord('abcde'.split(''), [0, 2, 1, 1, 0], 'abdcy')).toBeFalsy();
-                expect(solver.isPossibleWord('abcde'.split(''), [0, 2, 1, 1, 0], 'xcdby')).toBeFalsy();
-                expect(solver.isPossibleWord('abcde'.split(''), [0, 2, 1, 1, 0], 'xbfdc')).toBeFalsy();
-            });
-            test('scenario', () => {
-                expect(solver.isPossibleWord('basue'.split(''), [0,2,2,0,2], 'waste')).toBeTruthy();
-            });
-        });
         describe('evalPossibleWords', () => {
             test('green i should fail', () => {
-                expect(solver.evalPossibleWords('rinse', [1, 2, 1, 1, 0], currentWordList)).toEqual([]);
+                expect(evalPossibleWords('rinse', [1, 2, 1, 1, 0], currentWordList)).toEqual([]);
             });
             test('s, e are green but the a is black', () => {
-                let words = solver.evalPossibleWords('basge', [0, 0, 2, 0, 2], currentWordList);
+                let words = evalPossibleWords('basge', [0, 0, 2, 0, 2], currentWordList);
                 expect(words).toEqual(['taste', 'waste']);
-                words = solver.evalPossibleWords('basue', [0, 0, 2, 0, 2], currentWordList);
+                words = evalPossibleWords('basue', [0, 0, 2, 0, 2], currentWordList);
                 expect(words).toEqual(['taste', 'waste', 'wedge']);
             });
             test('only an l and an m', () => {
-                let words = solver.evalPossibleWords('xmgle', [0, 1, 1, 1, 0], currentWordList);
+                let words = evalPossibleWords('xmgle', [0, 1, 1, 1, 0], currentWordList);
                 expect(words).toEqual(['llama', 'mango']);
             });
         });
@@ -72,7 +31,7 @@ describe('solver tests', () => {
             const scores = [];
             solverData.possibleWords = currentWordList;
             test('follow through 4 lines', () => {
-                solver.updateSolver(guesses,scores, solverData);
+                updateSolver(guesses,scores, solverData);
                 expect(solverData.level).toBe(0);
                 expect(solverData).toEqual({
                     level: 0,
@@ -83,7 +42,7 @@ describe('solver tests', () => {
 
                 guesses.push('quick');
                 scores.push([2, 0, 0, 0, 0]);
-                solver.updateSolver(guesses,scores, solverData);
+                updateSolver(guesses,scores, solverData);
                 expect(solverData).toEqual({
                     level: 1,
                     possibleWords: currentWordList,
@@ -94,7 +53,7 @@ describe('solver tests', () => {
 
                 guesses.push('fixqa');
                 scores.push([0, 0, 0, 0, 0]);
-                solver.updateSolver(guesses,scores, solverData);
+                updateSolver(guesses,scores, solverData);
                 expect(solverData).toEqual({
                     level: 2,
                     possibleWords: ['taste', 'waste', 'llama', 'mango'],
@@ -103,7 +62,7 @@ describe('solver tests', () => {
 
                 guesses.push('abmdc');
                 scores.push([0, 0, 1, 0, 0]);
-                solver.updateSolver(guesses,scores, solverData);
+                updateSolver(guesses,scores, solverData);
                 expect(solverData).toEqual({
                     level: 3,
                     possibleWords: ['llama', 'mango'],
@@ -112,7 +71,7 @@ describe('solver tests', () => {
 
                 guesses.push('alarm');
                 scores.push([1, 1, 2, 0, 1]);
-                solver.updateSolver(guesses,scores, solverData);
+                updateSolver(guesses,scores, solverData);
                 expect(solverData).toEqual({
                     level: 4,
                     possibleWords: ['llama'],
